b659a9ec39bfff4f9a3996b8da98286aad7e3a48
at5200349-cyber <at5200349@gmail.com>
2026-08-19 00:29:37 +0530

    add user model and jwt token

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connetDB=async()=>{
     try {
       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}` )
       console.log(`\n MongoDB connected !! DB HOST ${connectionInstance.connection.host}`);
-        console.log(connectionInstance)
+      
     } catch (error) {
         console.log("MONGODB connection errr",error);
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv"
 import connetDB from "./db/index.js";
-
+import app from "./app.js"
 dotenv.config({path:'./.env'})
 
 connetDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is runninng at port : ${process.env.PORT}`);
+    })
+})
+.catch((e)=>{
+    console.log("connection failed ",e);
+})
 
 
 
