7ac19ce5d9091d9f778b8b0a00707e7ff54d5531
at5200349-cyber <at5200349@gmail.com>
2026-08-22 21:35:05 +0530

    crete user complete

--- src/app.js
+++ src/app.js
@@ -17,4 +17,19 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export {app}
+
+
+// routes import
+
+
+import userRouter from "./routes/user.route.js"
+
+
+
+
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
+
+export default app
