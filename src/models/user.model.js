52d7ebb2ee65b9e9bf963621270658548a923a6f
at5200349-cyber <at5200349@gmail.com>
2026-08-30 16:01:31 +0530

    add user account management features: change password, update details, and avatar

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,8 +50,8 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save",async function(next){
-    if(!this.isModified("password"))return ;
-        this.password=await bcrypt.hash(this.password,10)
+    if(!this.isModified("password"))return next();
+    this.password=await bcrypt.hash(this.password,10)
   
 })
 
