24a6bb555dac10dcad02eec9504c2fe66f603507
at5200349-cyber <at5200349@gmail.com>
2026-09-11 20:30:46 +0530

    add: implement toggle functionality for comments and tweets, update routes accordingly

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,8 @@ import { ApiResponse } from "../utils/ApiResponse";
 import { Video } from "../models/video.model";
 import { Comment } from "../models/comment.model";
 import { Like } from "../models/like.model";
+import {Tweet} from "../models/tweet.model";
+import mongoose from "mongoose";
 
 
 
@@ -41,5 +43,93 @@ const togglevideolike=asyncHandler(async(req,res)=>{
 
 })
 
+const togglecommentlike=asyncHandler(async(req,res)=>{
+        const {commentId}=req.params
+        const likeuser=req.user._id;
+        const comment=await Comment.findById(commentId);
+        if(!comment){
+            throw new ApiError(404,"Comment not found");
+        }
 
-export{togglevideolike};
+        const existlike=await Like.findOne({
+            comment:commentId,
+            likeby:likeuser,
+        });
+        if(existlike){
+            await Like.findByIdAndDelete(existlike._id);
+            return res.status(201).json(new ApiResponse(201,{},"Like toggle Successfully"));
+        }
+
+     const likedcomment=   await Like.create({
+            comment:commentId,
+            likeby:likeuser
+        })
+        if(!likedcomment){
+            throw new ApiError(500,"Internal server error");
+        }
+        return res.status(201).json(new ApiResponse(201,likedcomment,"Commment liked succssfully"));
+
+
+})
+
+const toggletweet= asyncHandler(async(req,res)=>{
+    const {tweetId}=req.params;
+    const tweet=await Tweet.findById(tweetId);
+    if(!tweet){
+        throw new ApiError(404,"Tweet not found");
+    }
+    const tweetby=req.user._id;
+
+    const existtweet=await Like.findOne({
+        tweet:tweetId,
+        likeby:tweetby
+    });
+    if(existtweet){
+        await Like.findByIdAndDelete(existtweet?._id);
+        throw new ApiResponse(201,{},"Like remved succefully");
+    }
+    const tweetcreate=await Like.create({
+        tweet:tweetId,
+        likeby:tweetby,
+
+        
+    })
+
+    return res.status(201).json(new ApiResponse(201,tweetcreate,"Like succefully"));
+
+
+})
+
+
+const getLikedvideos=asyncHandler(async(req,res)=>{
+        const userId=req.user._id;
+
+
+        const likedVideos=await Like.aggregate([
+            {
+                $match:{
+                    likeby:new mongoose.Types.ObjectId(userId),
+                    video:{$exists:true}
+                }
+            },
+            {
+                $lookup:{
+                    from:"videos",
+                    localField:"video",
+                    foreignField:"_id",
+                    as:"video"
+                }
+            },{
+                $unwind:"$video"
+            },{
+                $replaceRoot:{
+                    newRoot:"$video"
+                }
+            }
+        ]);
+
+        return res.status(200).json(new ApiResponse(200,likedVideos,"Liked videos fetched successfully"))
+})
+
+
+export{togglevideolike,togglecommentlike,toggletweet,getLikedvideos};
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 const router=Router();
 import { verifyJWT } from "../middlewares/auth.middleware";
-import { togglevideolike } from "../controllers/like.controller";
+import { togglecommentlike, togglevideolike,toggletweet, getLikedvideos } from "../controllers/like.controller";
 
 router.route("/toggle/v/:videoId").post(verifyJWT,togglevideolike);
+router.route("/toggle/c/commentId").post(verifyJWT,togglecommentlike);
+router.route("/toggle/t/tweetId").post(verifyJWT,toggletweet);
+router.route("/videos").get(verifyJWT,getLikedvideos)
 
 
 export default router
